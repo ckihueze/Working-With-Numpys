4db71f7a1d73746523dd32094d54a12626e2ee77
Chima Ihueze <kihueze@gmail.com>
2021-02-11 07:23:18 +0100

    accessing array elements

---
+++ accessing_array_elements.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+
+# Accessing the elements in the array using square brackets. When you’re accessing elements, remember that indexing in NumPy starts at 0.
+
+print(a[0])
+print(b[0])
