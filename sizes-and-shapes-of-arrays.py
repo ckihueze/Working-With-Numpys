177792e2c88e0427a0e63f028f818eb8a23d2079
Chima Ihueze <kihueze@gmail.com>
2021-02-12 21:32:34 +0100

    updated commit

--- sizes-and-shapes-of-arrays.py
+++ sizes-and-shapes-of-arrays.py
@@ -1,7 +1,24 @@
+import numpy as np  
+
+a = np.array([[[1, 2, 3, 7], [4, 5, 6, 2]], [[3, 5, 8, 9], [4, 5, 7, 2]], [[1, 2, 5, 6], [4, 7, 1, 9]]])
+b = np.array([[1, 2], [3, 4], [5, 6]])
+c = np.array([[1, 2, 3, 4, 7], [5, 6, 7, 2, 8], [9, 10, 11, 12, 19], [13, 14, 15, 20, 16]])
+
 # ndarray.ndim will tell you the number of axes, or dimensions, of the array.
-# ndarray.size will tell you the total number of elements of the array. 
-# This is the product of the elements of the array’s shape.
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+
+# ndarray.size will tell you the total number of elements of the array.
+
+print(a.size)
+print(b.size)
+print(c.size)
+
 # ndarray.shape will display a tuple of integers that indicate the number of elements stored along each dimension of the array.
 # If, for example, you have a 2-D array with 2 rows and 3 columns, the shape of your array is (2, 3).
 
-import numpy as np  
+print(a.shape)
+print(b.shape)
+print(c.shape)
