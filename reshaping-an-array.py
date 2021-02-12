6dbb1d3984f3fa796b7015c35d78d006c3ab54c0
Chima Ihueze <kihueze@gmail.com>
2021-02-12 23:19:17 +0100

    update

--- reshaping-an-array.py
+++ reshaping-an-array.py
@@ -4,3 +4,18 @@ import numpy as np
 # Just remember that when you use the reshape method, the array you want to produce needs to have the same number of elements as the original array.
 # If you start with an array with 12 elements, you’ll need to make sure that your new array also has a total of 12 elements.
 
+a = np.arange(12)
+print(a)
+print(" ")
+
+b = a.reshape(4, 3)
+print(b)
+print(" ")
+
+c = a.reshape(2, 3, 2)
+print(c)
+print(" ")
+
+d = a.reshape(2, 3, -1)
+print(d)
+print(" ")
