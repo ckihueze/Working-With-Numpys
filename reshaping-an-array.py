ea3c0da2bc59495e4e8584bcce89a8baf99e57a1
Chima Ihueze <kihueze@gmail.com>
2021-02-12 21:40:52 +0100

    initial commit

---
+++ reshaping-an-array.py
@@ -0,0 +1,6 @@
+import numpy as np  
+
+# Using arr.reshape() will give a new shape to an array without changing the data.
+# Just remember that when you use the reshape method, the array you want to produce needs to have the same number of elements as the original array.
+# If you start with an array with 12 elements, you’ll need to make sure that your new array also has a total of 12 elements.
+
