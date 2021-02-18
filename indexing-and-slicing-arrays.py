925449197493e54ecddf1fc87ba435b6de425c5a
Chima Ihueze <kihueze@gmail.com>
2021-02-18 10:30:13 +0100

    update commit

--- indexing-and-slicing-arrays.py
+++ indexing-and-slicing-arrays.py
@@ -3,6 +3,7 @@ import numpy as np
 # You can index and slice NumPy arrays in the same ways you can slice Python lists.
 
 a = np.array([1, 2, 3, 4, 5])
+
 print(a[1])
 print(a[-2])
 print(a[:3])
@@ -10,3 +11,4 @@ print(a[2:])
 print(a[1:4])
 print(a[:-1])
 print(a[:-2])
+print(a[-2:])
