3320c781c21b11a91eb115149e84de599b8f7814
Chima Ihueze <kihueze@gmail.com>
2021-02-22 19:40:32 +0100

    updated commit

--- copy-and-views.py
+++ copy-and-views.py
@@ -24,6 +24,15 @@ arr1[0] = 20
 print(arr1)
 print(b)
 
+# copies owns the data, and views does not own the data, but how can we check this?
+# Every NumPy array has the attribute base that returns None if the array owns the data.
+# Otherwise, the base attribute refers to the original object.
+
+x = arr.copy()
+y = arr.view()
+
+print(x.base)
+print(y.base)
 
 
 
