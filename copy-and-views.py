b617ac329453863b8dde828384997b39b77db360
Chima Ihueze <kihueze@gmail.com>
2021-02-22 19:22:00 +0100

    initial commit

--- copy-and-views.py
+++ copy-and-views.py
@@ -14,6 +14,14 @@ arr[0] = 20
 print(arr)
 print(a)
 
+# Make a view, change the original array, and display both arrays:
+
+arr1 = np.array([2, 1, 4, 7])
+b = arr.view()
+arr1[0] = 20
+print(arr1)
+print(b)
+
 
 
 
