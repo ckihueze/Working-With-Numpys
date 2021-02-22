921ea95ae5af1a8953009718387d3187db3afee1
Chima Ihueze <kihueze@gmail.com>
2021-02-22 19:57:35 +0100

    initial commit

---
+++ basic-array-operations.py
@@ -0,0 +1,9 @@
+# Once you’ve created your arrays, you can start to work with them.
+# Let’s say, for example, that you’ve created two arrays, one called “data” and one called “ones”
+
+import numpy as np
+
+data = np.array([2, 4])
+ones = np.ones(2, dtype = int)
+
+
