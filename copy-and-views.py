2614d240f6be532105307db587f9e76eadef9c2e
Chima Ihueze <kihueze@gmail.com>
2021-02-22 18:56:12 +0100

    initial commit

---
+++ copy-and-views.py
@@ -0,0 +1,7 @@
+# The main difference between a copy & a view of an array is that the copy is a new array, and the view is just a view of the original array.
+# Any changes made to the copy will not affect original array, and any changes made to the original array will not affect the copy.
+# Any changes made to the view will affect the original array, and any changes made to the original array will affect the copy.
+import numppy as np
+
+
+
