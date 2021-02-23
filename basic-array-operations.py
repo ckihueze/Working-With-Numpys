a468ae10540ec26d57985a768967e5928da448fa
Chima Ihueze <kihueze@gmail.com>
2021-02-23 14:59:34 +0100

    update commit

--- basic-array-operations.py
+++ basic-array-operations.py
@@ -23,3 +23,15 @@ print(data / data2)
 print(data.sum())
 print(data2.sum())
 
+# Sum of Array Elements Along the Axis
+# If we specify the axis value, the sum of elements along that axis is returned.
+# If the array shape is (X, Y) then the sum along 0-axis will be of shape (1, Y).
+# The sum along 1-axis will be of shape (1, X).
+
+arr = np.array([[1, 2,], [4, 5], [6, 7]])
+print(arr.sum(axis = 0))
+print(arr.sum(axis = 1))
+
+arr1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr1.sum(axis = 0))
+print(arr1.sum(axis = 1))
