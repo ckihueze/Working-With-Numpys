5b08158f939b14e09249b466b1c100a1eaaba471
Chima Ihueze <kihueze@gmail.com>
2021-02-23 16:57:07 +0100

    update commit

--- iterating-through-numpy-arrays.py
+++ iterating-through-numpy-arrays.py
@@ -30,3 +30,12 @@ for x in arr2:
     for y in x:
         for z in y:
             print(z)
+            
+# Iterating Arrays Using nditer()
+# The function nditer() is a helping function that can be used from very basic to very advanced iterations. 
+# It solves some basic issues which we face in iteration, lets go through it with examples.
+# In basic for loops, iterating through each scalar of an array we need to use n for loops which can be difficult to write for arrays with very high dimensionality.
+
+arr3 = np.array([[1, 2, 3], [4, 5, 6]])
+for x in np.nditer(arr3):
+    print(x)
