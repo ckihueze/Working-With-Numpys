77badd96dedb8961edd66749e56268ff4310d8d1
Chima Ihueze <kihueze@gmail.com>
2021-02-25 09:48:33 +0100

    initial commit

---
+++ splitting-numpy-arrays.py
@@ -0,0 +1,5 @@
+# Splitting is reverse operation of Joining.
+# Joining merges multiple arrays into one and Splitting breaks one array into multiple.
+# We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
+
+import numpy as np
