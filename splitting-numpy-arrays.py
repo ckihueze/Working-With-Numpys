c05e44a40bdf989b5e3490de74b63544e8fce930
Chima Ihueze <kihueze@gmail.com>
2021-02-25 10:04:47 +0100

    updated commit

--- splitting-numpy-arrays.py
+++ splitting-numpy-arrays.py
@@ -3,3 +3,14 @@
 # We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
 
 import numpy as np
+
+# split the array into 3 parts:
+
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(np.array_split(a, 3))
+
+# Split the array in 4 parts:
+# If the array has less elements than required, it will adjust from the end accordingly.
+
+b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(np.array_split(b, 4))
