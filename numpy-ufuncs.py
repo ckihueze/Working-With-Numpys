43ed1a59ad6edda622c1c5517ccf81f4ae5bacd8
Chima Ihueze <kihueze@gmail.com>
2021-02-28 13:06:39 +0100

    initial commit

---
+++ numpy-ufuncs.py
@@ -0,0 +1,17 @@
+# ufuncs stands for "Universal Functions" and they are NumPy functions that operates on the ndarray object.
+# ufuncs are used to implement vectorization in NumPy which is way faster than iterating over elements.
+# They also provide broadcasting and additional methods like reduce, accumulate etc. that are very helpful for computation.
+# ufuncs also take additional arguments, like:
+# where boolean array or condition defining where the operations should take place.
+# dtype defining the return type of elements.
+# out output array where the return value should be copied.
+
+# What is Vectorization?
+# Converting iterative statements into a vector based operation is called vectorization.
+# It is faster as modern CPUs are optimized for such operations.
+# Add the Elements of Two Lists
+# list 1: [1, 2, 3, 4]
+# list 2: [4, 5, 6, 7]
+
+# One way of doing it is to iterate over both of the lists and then sum each elements.
+
