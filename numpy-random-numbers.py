603f5edb7ec4dc4a14b28e5dd4552bea1d30f63a
Chima Ihueze <kihueze@gmail.com>
2021-02-28 08:15:08 +0100

    updated commit

--- numpy-random-numbers.py
+++ numpy-random-numbers.py
@@ -22,3 +22,10 @@ print(x)
 y = random.rand()
 print(y)
 
+# Generate Random Array
+# In NumPy we work with arrays, and you can use the two methods from the above examples to make random arrays.
+# The randint() method takes a size parameter where you can specify the shape of an array.
+
+# Generate a 1-D array containing 5 random integers from 0 to 100:
+
+
